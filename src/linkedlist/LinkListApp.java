bbc745aee84bd0dfb236217f3c115867d744bb45
Petr <pmerkuryev@gmail.com>
2025-06-17 21:26:30 +0200

    class Link implemented

--- src/linkedlist/LinkListApp.java
+++ src/linkedlist/LinkListApp.java
@@ -1,5 +1,19 @@
 package linkedlist;
 
+class Link {
+    public int iData;
+    public double dData;
+    public Link next;
+
+    public Link(int id, double dd) {
+        this.dData = dd;
+        this.iData = id;
+    }
+    public void displayLink() {
+        System.out.print("{"  + iData + ", " + dData + "} ");
+    }
+}
+
 public class LinkListApp {
     public static void main(String[] args) {
         System.out.println();
