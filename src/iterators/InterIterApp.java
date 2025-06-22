96f74b6908fa9f372eef5f05a7d271cb9ba16e35
Petr <pmerkuryev@gmail.com>
2025-06-22 20:50:31 +0200

    Iterator implemented

--- src/iterators/InterIterApp.java
+++ src/iterators/InterIterApp.java
@@ -1,5 +1,9 @@
 package iterators;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 class Link {
     public long dData;
     public Link next;
@@ -52,10 +56,134 @@ class ListIterator {
         current = ourList.getFirst();
         previous = null;
     }
-
+    public boolean atEnd() {
+        return current.next == null;
+    }
+    public void nextLink() {
+        previous = current;
+        current = current.next;
+    }
+    public Link getCurrent() {
+        return current;
+    }
+    public void insertAfter(long dd) {
+        Link newLink = new Link(dd);
+        if (ourList.isEmpty()) {
+            ourList.setFirst(newLink);
+            reset();
+        } else {
+            newLink.next = current.next;
+            current.next = newLink;
+            nextLink();
+        }
+    }
+    public void insertBefore(long dd) {
+        Link newLink = new Link(dd);
+        if (previous == null) { //beginning of list or empty list
+            newLink.next = ourList.getFirst();
+            ourList.setFirst(newLink);
+            reset();
+        } else {
+            newLink.next = previous.next;
+            previous.next = newLink;
+            current = newLink;
+        }
+    }
+    public long deleteCurrent() {
+        long value = current.dData;
+        if (previous == null){
+            ourList.setFirst(current.next);
+            reset();
+        } else {
+            previous.next = current.next;
+            if (atEnd())
+                reset();
+            else
+                current = current.next;
+        }
+        return value;
+    }
 }
 public class InterIterApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        LinkList theList = new LinkList();
+        ListIterator iter1 = theList.getIterator();
+        long value;
 
+        iter1.insertAfter(20);
+        iter1.insertAfter(40);
+        iter1.insertAfter(80);
+        iter1.insertBefore(60);
+
+        theList.displayList();
+        while (true) {
+            System.out.println("Enter of first letter of show, reset, ");
+            System.out.println("next, get, before, after, delete: ");
+
+            int choice = getChar();
+            switch (choice) {
+                case 's':       //show list
+                    if (!theList.isEmpty())
+                        theList.displayList();
+                    else
+                        System.out.println("List is empty");
+                    break;
+                case 'r':
+                    iter1.reset();
+                    break;
+                case 'n':
+                    if (!theList.isEmpty() && !iter1.atEnd()) {
+                        iter1.nextLink();
+                    } else {
+                        System.out.println("Can't go to next link");
+                    }
+                    break;
+                case 'g':
+                    if (!theList.isEmpty()) {
+                        value = iter1.getCurrent().dData;
+                        System.out.println("Returned: " + value);
+                    } else {
+                        System.out.println("List is empty");
+                    }
+                    break;
+                case 'b': //insert before
+                    System.out.println("Enter value to insert");
+                    value = getInt();
+                    iter1.insertBefore(value);
+                    break;
+                case 'a':
+                    System.out.println("Enter value to insert");
+                    value = getInt();
+                    iter1.insertAfter(value);
+                    break;
+                case 'd':
+                    if (!theList.isEmpty()) {
+                        value = iter1.deleteCurrent();
+                        System.out.println("Deleted " + value);
+                    } else {
+                        System.out.println("Can't delete");
+                    }
+                    break;
+                case ' ':
+                    return;
+                default:
+                    System.out.println("Invalid entry");
+            }
+
+        }
+    }
+    public static String getString() throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        return br.readLine();
+    }
+    public static char getChar() throws IOException {
+        String s = getString();
+        return s.charAt(0);
+
+    }
+    public static int getInt() throws IOException {
+        String s = getString();
+        return Integer.parseInt(s);
     }
 }
